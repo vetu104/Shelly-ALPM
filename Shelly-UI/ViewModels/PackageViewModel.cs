55174dd862c34f3c1fad3edcf535ae8d78cd4bee
Zoey <zoey.erin.bauer@gmail.com>
2026-01-08 17:50:28 -0500

    Add search and filtering functionality for packages in PackageViewModel; integrate DataGrid for displaying results in PackageWindow

--- Shelly-UI/ViewModels/PackageViewModel.cs
+++ Shelly-UI/ViewModels/PackageViewModel.cs
@@ -1,4 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive.Linq;
+using PackageManager.Alpm;
 using ReactiveUI;
 
 namespace Shelly_UI.ViewModels;
@@ -6,8 +11,45 @@ namespace Shelly_UI.ViewModels;
 public class PackageViewModel : ViewModelBase, IRoutableViewModel
 {
     public IScreen HostScreen { get; }
+    private AlpmManager _alpmManager = new AlpmManager();
+    private string? _searchText;
+    private readonly ObservableAsPropertyHelper<IEnumerable<AlpmPackage>> _filteredPackages;
 
-    public PackageViewModel(IScreen screen) => HostScreen = screen;
-    
+    public PackageViewModel(IScreen screen)
+    {
+        HostScreen = screen;
+        _alpmManager.IntializeWithSync();
+        AvailablePackages = new ObservableCollection<AlpmPackage>(_alpmManager.GetAvailablePackages());
+
+        _filteredPackages = this
+            .WhenAnyValue(x => x.SearchText)
+            .Throttle(TimeSpan.FromMilliseconds(250))
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .Select(Search)
+            .ToProperty(this, x => x.FilteredPackages);
+    }
+
+    private IEnumerable<AlpmPackage> Search(string? searchText)
+    {
+        if (string.IsNullOrWhiteSpace(searchText))
+        {
+            return AvailablePackages;
+        }
+
+        return AvailablePackages.Where(p => 
+            p.Name.Contains(searchText, StringComparison.OrdinalIgnoreCase) ||
+            p.Version.Contains(searchText, StringComparison.OrdinalIgnoreCase));
+    }
+
     public string UrlPathSegment { get; } = Guid.NewGuid().ToString().Substring(0, 5);
+
+    public ObservableCollection<AlpmPackage> AvailablePackages { get; set; }
+
+    public IEnumerable<AlpmPackage> FilteredPackages => _filteredPackages.Value;
+
+    public string? SearchText
+    {
+        get => _searchText;
+        set => this.RaiseAndSetIfChanged(ref _searchText, value);
+    }
 }
